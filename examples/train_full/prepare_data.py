e255ed1073dab5d1e601b9bdc64d460070541af7
hexuan21 <hex9269@gmail.com>
2025-07-17 14:14:21 +0000

    update

--- examples/train_full/prepare_data.py
+++ examples/train_full/prepare_data.py
@@ -7,6 +7,7 @@ Usage:
 """
 
 import os
+import shutil
 import json
 import argparse
 import requests
@@ -38,7 +39,7 @@ def download_file(url: str, save_path: str, overwrite: bool = False, timeout: in
     print(f"[ok] Downloaded → {save_path}")
 
 
-def update_dataset_info(sft_data_name: str, data_file: str, ds_info_path: str = "data/dataset_info.json"):
+def update_dataset_info(sft_data_name: str, data_file: str, ds_info_path: str = "data/dataset_info.json", frame_or_video: str = "video"):
     os.makedirs(os.path.dirname(ds_info_path), exist_ok=True)
 
     if os.path.exists(ds_info_path):
@@ -46,47 +47,62 @@ def update_dataset_info(sft_data_name: str, data_file: str, ds_info_path: str = 
             ds_infos = json.load(f)
     else:
         ds_infos = {}
-
-    ds_infos[sft_data_name] = {
-        "file_name": data_file,
-        "formatting": "sharegpt",
-        "columns": {
-            "messages": "conversations",
-            "videos": "videos"
+    if frame_or_video in ["videos","video","v"]:
+        ds_infos[sft_data_name] = {
+            "file_name": data_file,
+            "formatting": "sharegpt",
+            "columns": {
+                "messages": "conversations",
+                "videos": "videos"
+            }
         }
-    }
-
+        
+    else:
+        ds_infos[sft_data_name] = {
+            "file_name": data_file,
+            "formatting": "sharegpt",
+            "columns": {
+                "messages": "conversations",
+                "images": "images"
+            }
+        }
     with open(ds_info_path, "w", encoding="utf-8") as f:
         json.dump(ds_infos, f, indent=2, ensure_ascii=False)
     print(f"[ok] dataset_info.json updated → {ds_info_path}")
 
 
-def main(sft_data_name: str):
+def main(sft_data_name: str, frame_or_video: str):
     data_file  = f"{sft_data_name}.json"
-    video_zip  = f"{sft_data_name}.zip"
-
-    data_url = f"https://huggingface.co/datasets/{HF_DATASET_USER}/{HF_DATASET_NAME}/resolve/main/{data_file}"
-    zip_url = f"https://huggingface.co/datasets/{HF_DATASET_USER}/{HF_VIDEO_NAME}/resolve/main/{video_zip}"
-
     data_save = os.path.join("data", data_file)
-    zip_save = os.path.join("data", video_zip)
-    video_dir = os.path.join("data", "videos")
-
-    download_file(data_url, data_save)
+    
+    if frame_or_video in ["frames","frame","f"]:
+        zip_file  = f"{sft_data_name}_frames.zip"
+        f_v_dir = os.path.join("data", "frames")
+    if frame_or_video in ["videos","video","v"]:
+        zip_file  = f"{sft_data_name}_videos.zip"
+        f_v_dir = os.path.join("data", "videos")
+    zip_save = os.path.join("data", zip_file)
+    
+    data_url = f"https://huggingface.co/datasets/{HF_DATASET_USER}/{HF_DATASET_NAME}/resolve/main/{data_file}"
+    zip_url = f"https://huggingface.co/datasets/{HF_DATASET_USER}/{HF_VIDEO_NAME}/resolve/main/{zip_file}"
+    
+    download_file(data_url, data_save, overwrite=True)
     with open(data_save, "r", encoding="utf-8") as f:
         data = json.load(f)
-        data = [x for x in data if '000241_a.mp4' not in x['videos'][0]]
     with open(data_save, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
-    download_file(zip_url, zip_save)
+    download_file(zip_url, zip_save, overwrite=True)
 
     update_dataset_info(sft_data_name, data_file)
 
-    os.makedirs(video_dir, exist_ok=True)
+    os.makedirs(f_v_dir, exist_ok=True)
     try:
+        if os.path.exists(f_v_dir):
+            shutil.rmtree(f_v_dir)
+        os.makedirs(f_v_dir, exist_ok=True)
         with ZipFile(zip_save) as zf:
-            zf.extractall(video_dir)
-        print(f"[ok] Unzipped → {video_dir}")
+            zf.extractall(f_v_dir)
+        print(f"[ok] Unzipped → {f_v_dir}")
     except BadZipFile as e:
         print(f"[error] Bad zip file: {e}")
     os.remove(zip_save)
@@ -94,5 +110,6 @@ def main(sft_data_name: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Prepare SFT JSON & ZIP")
     parser.add_argument("--sft_data_name", required=True)
+    parser.add_argument("--frame_or_video", required=True)
     args = parser.parse_args()
-    main(args.sft_data_name)
+    main(args.sft_data_name, args.frame_or_video)
--- examples/train_full/run.sh
+++ examples/train_full/run.sh
@@ -3,27 +3,23 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-HUB_USER_NAME="videoscore2_exp"
-CURR_MODEL_NAME="vs2_qwen2_5vl_sft_17k"
 DATASET_NAME="sft_17k"
-HUB_MODEL_ID="${HUB_USER_NAME}/${CURR_MODEL_NAME}"
+# DATASET_NAME="try_debug"
 
 # read from .bashrc or set explicitly
-# WANDB_TOKEN="${WANDB_TOKEN:?Set WANDB_TOKEN}"
+WANDB_TOKEN="${WANDB_TOKEN:?Set WANDB_TOKEN}"
 HF_TOKEN="${HF_TOKEN:?Set HF_TOKEN}"
 
 # echo "Download SFT data and videos. Update dataset_info.json"
 # echo "Current SFT data name: ${DATASET_NAME}"
 # python examples/train_full/prepare_data.py \
-#     --sft_data_name ${DATASET_NAME}
+#     --sft_data_name ${DATASET_NAME} \
+#     --frame_or_video "v"
 
-# wandb login --relogin $WANDB_TOKEN
-# echo "WandB login."
+wandb login --relogin $WANDB_TOKEN
+echo "WandB login."
 
 echo "Start training ..."
-llamafactory-cli train examples/train_full/qwen2_5vl_7b_full_sft.yaml \
-    dataset=${DATASET_NAME} \
-    output_dir="saves/${CURR_MODEL_NAME}" \
-    export_hub_model_id=${HUB_MODEL_ID} \
+llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_17k_1e-5_bs64.yaml \
     hf_hub_token=${HF_TOKEN} \
-
+    dataset=${DATASET_NAME} 
