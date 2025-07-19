eb270b3b75a27598bca51c57b091deb5b5d849e8
hexuan21 <hex9269@gmail.com>
2025-07-19 02:50:33 +0000

    add yaml for larger cutoff_len

--- examples/train_full/install.sh
+++ examples/train_full/install.sh
@@ -2,9 +2,10 @@
 
 set -x
 
-# conda create -n lmfac python=3.10 -y
+# conda create -n lmfac python=3.11 -y
 # conda activate lmfac
 
 pip install -e ".[torch,metrics]" --no-build-isolation
 pip install wandb
 pip install deepspeed==0.16.9
+pip install --no-deps transformers==4.50.0
