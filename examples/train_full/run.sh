fa19adfd8172461efac63279e23805fe1156b0d7
hexuan21 <hex9269@gmail.com>
2025-07-28 03:50:27 +0000

    update

--- examples/train_full/run.sh
+++ examples/train_full/run.sh
@@ -10,16 +10,16 @@ DATASET_NAME="sft_17k"
 WANDB_TOKEN="${WANDB_TOKEN:?Set WANDB_TOKEN}"
 HF_TOKEN="${HF_TOKEN:?Set HF_TOKEN}"
 
-echo "Download SFT data and videos. Update dataset_info.json"
-echo "Current SFT data name: ${DATASET_NAME}"
-python examples/train_full/prepare_data.py \
-    --sft_data_name ${DATASET_NAME} \
-    --frame_or_video "v"
+# echo "Download SFT data and videos. Update dataset_info.json"
+# echo "Current SFT data name: ${DATASET_NAME}"
+# python examples/train_full/prepare_data.py \
+#     --sft_data_name ${DATASET_NAME} \
+#     --frame_or_video "v"
 
-# wandb login --relogin $WANDB_TOKEN
-# echo "WandB login."
+wandb login --relogin $WANDB_TOKEN
+echo "WandB login."
 
-# echo "Start training ..."
-# llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_17k_1e-5_4fps_8192.yaml \
-#     hf_hub_token=${HF_TOKEN} \
-#     dataset=${DATASET_NAME} 
+echo "Start training ..."
+llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_17k_1e-5_4fps_8192.yaml \
+    hf_hub_token=${HF_TOKEN} \
+    dataset=${DATASET_NAME} 
