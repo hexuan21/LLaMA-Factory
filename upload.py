c86a39a9695d4a5512641454f37ee194f9a93c80
hexuan21 <hex9269@gmail.com>
2025-07-31 01:40:53 +0000

    update

---
+++ upload.py
@@ -0,0 +1,50 @@
+import os
+from huggingface_hub import HfApi, upload_file
+from huggingface_hub.utils import RepositoryNotFoundError
+import argparse
+
+def upload_files_individually_to_hub(
+    local_dir: str,
+):
+    api = HfApi()
+    model_name=local_dir.split('/')[-1]
+    repo_id=f"videoscore2/{model_name}"
+    token=os.environ["HF_TOKEN"]
+    repo_type="model"
+    try:
+        api.repo_info(repo_id=repo_id, repo_type=repo_type)
+        print(f"✅ Repository '{repo_id}' already exists.")
+    except RepositoryNotFoundError:
+        print(f"🚧 Repository '{repo_id}' not found. Creating a new public repo...")
+        api.create_repo(
+            repo_id=repo_id,
+            token=token,
+            repo_type=repo_type,
+            private=False
+        )
+
+    for root, _, files in os.walk(local_dir):
+        for file in files:
+            abs_path = os.path.join(root, file)
+            relative_path = os.path.relpath(abs_path, local_dir)  # path inside repo
+            print(f"Uploading {relative_path} ...")
+
+            upload_file(
+                path_or_fileobj=abs_path,
+                path_in_repo=relative_path,
+                repo_id=repo_id,
+                repo_type=repo_type,
+                token=token,
+                commit_message=f"Upload {relative_path}"
+            )
+
+    print(f"✅ All files in '{local_dir}' have been uploaded to {repo_id}.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_dir", type=str, required=True, help="Local folder path to upload")
+    args = parser.parse_args()
+
+    upload_files_individually_to_hub(
+        local_dir=args.local_dir,
+    )
