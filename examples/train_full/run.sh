4c460f3c718157e37fd78d117b6c7560c7294a64
hexuan21 <hex9269@gmail.com>
2025-09-11 16:35:16 +0000

    update

--- examples/train_full/run.sh
+++ examples/train_full/run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DATASET_NAME="sft_17k"
+DATASET_NAME="sft_25k"
 # DATASET_NAME="try_debug"
 
 # read from .bashrc or set explicitly
@@ -13,13 +13,13 @@ HF_TOKEN="${HF_TOKEN:?Set HF_TOKEN}"
 # echo "Download SFT data and videos. Update dataset_info.json"
 # echo "Current SFT data name: ${DATASET_NAME}"
 # python examples/train_full/prepare_data.py \
-#     --sft_data_name ${DATASET_NAME} \
+#     --sft_data_name "sft_25k" \
 #     --frame_or_video "v"
 
 wandb login --relogin $WANDB_TOKEN
 echo "WandB login."
 
 echo "Start training ..."
-llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_17k_1e-5_4fps_8192.yaml \
+CUDA_VISIBLE_DEVICES=1,2,3,4 llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_17k_2e-4_2fps_960_720_8192.yaml \
     hf_hub_token=${HF_TOKEN} \
     dataset=${DATASET_NAME} 
