82368de4fb0f5b968d3ea65c25075e33dc11edba
hexuan21 <hex9269@gmail.com>
2025-09-15 05:31:33 +0000

    update

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DATASET_NAME="sft_25k"
+DATASET_NAME="sft_27k"
 # DATASET_NAME="try_debug"
 
 # read from .bashrc or set explicitly
@@ -20,6 +20,6 @@ wandb login --relogin $WANDB_API_KEY
 echo "WandB login."
 
 echo "Start training ..."
-CUDA_VISIBLE_DEVICES=1,2,3,4 llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_25k_2e-4_2fps_960_720_8192.yaml \
+llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_27k_2e-4_2fps_960_720_8192.yaml \
     hf_hub_token=${HF_TOKEN} \
     dataset=${DATASET_NAME} 
