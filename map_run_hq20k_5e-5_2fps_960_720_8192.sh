bde932574636776bda849337befb82ba93081500
hexuan21 <hex9269@gmail.com>
2025-09-20 16:12:35 +0000

    update

--- map_run_hq20k_5e-5_2fps_960_720_8192.sh
+++ map_run_hq20k_5e-5_2fps_960_720_8192.sh
@@ -19,6 +19,6 @@ export HF_HOME='/map-vepfs/huggingface'
 
 wandb login --relogin $WANDB_API_KEY
 echo "Start training..."
-llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_27k_5e-5_2fps_960_720_8192.yaml \
+llamafactory-cli train examples/train_full/vs2_qwen2_5vl_sft_hq_20k_5e-5_2fps_960_720_8192.yaml \
     hf_hub_token=$HF_TOKEN \
     dataset=sft_hq_20k
